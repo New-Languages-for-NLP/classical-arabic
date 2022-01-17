76927f9d9de25a197a6b4ee39f4ddb008a6cc846
Maroussia <maroussia.b@gmail.com>
2022-01-17 16:21:16 +0100

    Replaces Classical_arabic by ClassicalArabic

--- 2_new_language_object/setup.py
+++ 2_new_language_object/setup.py
@@ -3,6 +3,6 @@ from setuptools import setup
 setup(
     name="clara",
     entry_points={
-        "spacy_languages": ["clara = clara:Classical_arabic"],
+        "spacy_languages": ["clara = clara:ClassicalArabic"],
     }
 )
